c7461f24c767d59b4009a4ccb9a1cceffedb37de
dqunbp <dqunbp@MacBook-Pro-dqunbp.local>
2019-01-18 18:24:53 +0300

    update backendUrl

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const isProduction = process.env.NODE_ENV === "production";
 export const {
   REACT_APP_BACKEND_URL: backendUrl = isProduction
-    ? `${window.location.protocol}//${window.location.host}/api`
+    ? `${window.location.protocol}//${window.location.host}`
     : "http://localhost:5000"
 } = process.env;
